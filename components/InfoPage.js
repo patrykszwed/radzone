f31f15c2704effa64bc258c11ebb10000f205f1b
patryk.szwed@outlook.com <patryk.szwed@outlook.com>
2020-05-05 00:25:30 +0200

    Removed not used styles

--- components/InfoPage.js
+++ components/InfoPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {
   Image,
-  Platform,
   ScrollView,
   StyleSheet,
   Text,
@@ -85,17 +84,6 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     justifyContent: "space-around",
   },
-  backButton: {
-    marginTop: 30,
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center",
-    padding: 50,
-  },
   contentContainer: {
     paddingTop: 30,
   },
@@ -103,7 +91,6 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: "rgba(96,100,109, 1)",
     textAlign: "center",
-    // color: "#000",
     fontWeight: "bold",
     margin: 15,
   },
@@ -111,94 +98,11 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "rgba(96,100,109, 1)",
     textAlign: "center",
-    // color: "#000",
     padding: 10,
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10,
-    marginBottom: 60,
   },
   mainImage: {
     width: 70,
     height: 40,
     resizeMode: "contain",
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)",
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center",
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center",
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center",
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7",
-  },
-  inputText: {
-    borderColor: "gray",
-    borderWidth: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    height: 50,
-    borderRadius: 20,
-    padding: 10,
   },
 });
