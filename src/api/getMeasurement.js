d9bbf63eb7157f99915b772a793f84193e74c463
Jarek Danielak <danielak.jarek@gmail.com>
2020-05-07 00:03:58 +0200

    Add modul for fetching measurement from API

---
+++ src/api/getMeasurement.js
@@ -0,0 +1,31 @@
+import "isomorphic-fetch";
+
+/**
+ * Function for fetching measurements at given point from Safecast API
+ * @param {float} latitude - point latitude
+ * @param {float} longitude - point longitude
+ * @param {float} distance - accepted range around point (100 is default)
+ * @returns Array of measurements objects with: measured value, unit and timestamp
+ **/
+export default async function (latitude, longitude, distance = 100) {
+  try {
+    return fetch(`https://api.safecast.org/measurements.json?distance=${distance}&latitude=${latitude}&longitude=${longitude}`)
+    .then((response) => response.json())
+    .then((json) => {
+      let result = [];
+      for (let item of Object.entries(json)) {
+        result.push({
+          value: item[1].value,
+          unit: item[1].unit,
+          timestamp: item[1].captured_at
+        });
+      }
+      return result;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
