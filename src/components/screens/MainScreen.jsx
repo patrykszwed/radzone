e4bf9ae1f4c21fd0e53df84c090f6c802727c98b
patryk.szwed@outlook.com <patryk.szwed@outlook.com>
2020-05-18 23:21:17 +0200

    Load data from API. Refresh data on button press.

--- src/components/screens/MainScreen.jsx
+++ src/components/screens/MainScreen.jsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
 
-import Actions from '../molecules/Actions';
-import RadiationValue from '../molecules/RadiationValue';
-import Indicator from '../molecules/Indicator';
+import Actions from "../molecules/Actions";
+import RadiationValue from "../molecules/RadiationValue";
+import Indicator from "../molecules/Indicator";
 
-import { CPM, USV, ROENTGEN } from '../../constants/units';
+import { CPM, USV, ROENTGEN } from "../../constants/units";
+import getMeasurement from "../../api/getMeasurement";
+
+import * as Location from "expo-location";
 
 const CPM_USV_MULTIPLIER = 30;
 const USV_ROENTGEN_MULTIPLIER = 8770;
@@ -15,10 +18,11 @@ const FATAL_RADIATION_POISONING_VALUE = 2000000;
 
 class MainScreen extends Component {
   state = {
-    // TO BE CHANGED TO FETCHED FROM BE
-    radiationValue: '44.00',
+    radiationValue: null,
     unit: CPM,
-  }
+    location: null,
+    errorMsg: "",
+  };
 
   convertValue = (currentUnit, unitToBeConvertedTo, value) => {
     if (currentUnit === CPM && unitToBeConvertedTo === USV) {
@@ -33,20 +37,36 @@ class MainScreen extends Component {
       return (value * (1 / CPM_ROENTGEN_MULTIPLIER)).toFixed(8);
     } else if (currentUnit === ROENTGEN && unitToBeConvertedTo === CPM) {
       return (value * CPM_ROENTGEN_MULTIPLIER).toFixed(2);
-    };
-  }
+    }
+  };
 
   onToUnitConvert = (unitToBeConvertedTo) => {
     const { radiationValue, unit } = this.state;
     if (unit === unitToBeConvertedTo) {
       return;
     }
-    const newValue = this.convertValue(unit, unitToBeConvertedTo, radiationValue)
+    const newValue = this.convertValue(
+      unit,
+      unitToBeConvertedTo,
+      radiationValue
+    );
     this.setState({
       unit: unitToBeConvertedTo,
       radiationValue: newValue,
-    })
-  }
+    });
+  };
+
+  onRefreshData = () => {
+    const { location } = this.state;
+    if (location) {
+      const { coords } = location;
+      this.setState({ radiationValue: null });
+      getMeasurement(coords.latitude, coords.longitude).then((result) => {
+        const { value } = result[0];
+        this.setState({ radiationValue: value });
+      });
+    }
+  };
 
   calculatePercentage = () => {
     const { radiationValue, unit } = this.state;
@@ -54,9 +74,23 @@ class MainScreen extends Component {
     if (unit !== USV) {
       sievertValue = this.convertValue(unit, USV, radiationValue);
     }
-    const percentage = ((sievertValue * HOURS_IN_YEAR) / FATAL_RADIATION_POISONING_VALUE) * 100;
+    const percentage =
+      ((sievertValue * HOURS_IN_YEAR) / FATAL_RADIATION_POISONING_VALUE) * 100;
     return percentage;
-  }
+  };
+
+  componentDidMount = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+    if (status === "granted") {
+      const location = await Location.getCurrentPositionAsync({});
+      const { coords } = location;
+
+      getMeasurement(coords.latitude, coords.longitude).then((result) => {
+        const { value } = result[0];
+        this.setState({ radiationValue: value, location });
+      });
+    }
+  };
 
   render() {
     const { radiationValue, unit } = this.state;
@@ -66,14 +100,12 @@ class MainScreen extends Component {
           <Indicator percentage={this.calculatePercentage()} />
         </View>
         <View style={styles.valueContainer}>
-          <RadiationValue
-            value={radiationValue}
-            unit={unit}
-          />
+          <RadiationValue value={radiationValue} unit={unit} />
         </View>
         <View style={styles.actionsContainer}>
           <Actions
             convertToUnit={this.onToUnitConvert}
+            refreshData={this.onRefreshData}
           />
         </View>
       </View>
@@ -84,22 +116,22 @@ class MainScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   indicatorContainer: {
     flex: 1.5,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
   valueContainer: {
     flex: 0.6,
-    alignItems: 'center',
+    alignItems: "center",
   },
   actionsContainer: {
     flex: 0.5,
-    alignItems: 'center',
-  }
+    alignItems: "center",
+  },
 });
 
 export default MainScreen;
